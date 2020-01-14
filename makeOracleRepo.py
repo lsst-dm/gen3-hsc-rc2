08afb3c05ca289f06f86536347106fb5d59627bb
Jim Bosch <jbosch@astro.princeton.edu>
2020-01-14 09:56:03 -0600

    Update makeOracleRepo.py to not use registry.cls.

--- makeOracleRepo.py
+++ makeOracleRepo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+import sys
+
 from lsst.daf.butler import Butler, Config
 
 config = Config()
-config[".registry.cls"] = "lsst.daf.butler.registries.oracleRegistry.OracleRegistry"
 config[".registry.db"] = "oracle+cx_oracle://@gen3_cred_jbosch_1"
 
-root = "/project/jbosch/gen3/RC2/repo-oracle"
+root = sys.argv[1]
 Butler.makeRepo(root, config=config)
