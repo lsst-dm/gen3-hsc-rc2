0b6ebcc65faea06f2b57b5690a87407e6a6234f2
Jim Bosch <jbosch@astro.princeton.edu>
2020-05-12 11:28:38 -0500

    Add yaml pipeline description.

--- pipeline.sh
+++ pipeline.sh
@@ -6,7 +6,4 @@
 # Or this to get remote debugging with VSCode (for jbosch):
 ## python -m ptvsd --host localhost --port 25689 --wait $CTRL_MPEXEC_DIR/bin/
 
-pipetask -i 'calibs/hsc/default','raw/hsc','refcats/ps1_pv3','skymaps','masks/hsc' \
-"$@" \
---register-dataset-types \
--p "$PIPE_TASKS_DIR"/pipelines/DataReleaseProduction.yaml
+pipetask "$@" -i 'calib/hsc','raw/hsc','refcats','masks/hsc','skymaps' -p pipelines/HSC-RC2.yaml
