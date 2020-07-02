e956223c39648df579aae32dfd3fe20ddb29332c
Jim Bosch <jbosch@astro.princeton.edu>
2020-07-02 10:33:03 -0500

    Add script to create personal PostgreSQL repo.
    And default to username instead of "jbosch" in the Oracle script, too.

--- makePostgresRepo.py
+++ makePostgresRepo.py
@@ -5,8 +5,11 @@ import getpass
 
 from lsst.daf.butler import Butler, Config
 
+username = getpass.getuser()
+
 config = Config()
-config[".registry.db"] = f"oracle+cx_oracle://@gen3_cred_{getpass.getuser()}_1"
+config[".registry.db"] = f"postgresql://{username}@lsst-pg-prod1.ncsa.illinois.edu:5432/lsstdb1"
+config[".registry.namespace"] = username
 
 root = sys.argv[1]
 Butler.makeRepo(root, config=config)
